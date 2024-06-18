80816c03edddc121ebf6fbb620d6d52619dfa94f
DonghyoHong <gogoehdgy@naver.com>
2024-06-18 14:28:19 +0900

    # 구구단 추가

---
+++ src/gugudan/Gugudan.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>{{ first }} 곱하기 {{ second }} 는 ??</div>
+  <form @submit="submitForm">
+    <input type="number" v-model="value"/>
+    <button>입력</button>
+  </form>
+  <div id="result">{{ result }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      first: Math.ceil(Math.random() * 9)
+      , second: Math.ceil(Math.random() * 9)
+      , value: ''
+      , result: ''
+    }
+  }
+  , methods: {
+    submitForm(e) {
+      e.preventDefault();
+      if (this.first * this.second === this.value) {
+        this.result = "정답";
+        this.first = Math.ceil(Math.random() * 9);
+        this.second = Math.ceil(Math.random() * 9);
+        this.value = '';
+      } else {
+        this.result = "땡";
+        this.value = '';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
