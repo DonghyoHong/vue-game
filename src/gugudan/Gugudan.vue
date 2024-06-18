4c59aec68d894613e8517c49e1b156a6346e21f5
DonghyoHong <gogoehdgy@naver.com>
2024-06-18 14:31:43 +0900

    # ref 추가

--- src/gugudan/Gugudan.vue
+++ src/gugudan/Gugudan.vue
@@ -1,7 +1,7 @@
 <template>
   <div>{{ first }} 곱하기 {{ second }} 는 ??</div>
   <form @submit="submitForm">
-    <input type="number" v-model="value"/>
+    <input type="number" v-model="value" ref="answer"/>
     <button>입력</button>
   </form>
   <div id="result">{{ result }}</div>
@@ -25,9 +25,11 @@ export default {
         this.first = Math.ceil(Math.random() * 9);
         this.second = Math.ceil(Math.random() * 9);
         this.value = '';
+        this.$refs.answer.focus();
       } else {
         this.result = "땡";
         this.value = '';
+        this.$refs.answer.focus();
       }
     }
   }
