77952c3686a8554469d8989397d4a1f0d29c859d
DonghyoHong <gogoehdgy@naver.com>
2024-06-18 16:07:02 +0900

    # 끝말잇기

---
+++ src/wordcaingame/ParentWordChainGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+// setup(){
+  data() {
+    return {}
+  }
+}
+</script>
+
+<!--<style lang="scss" scoped>-->
+<style scoped>
+
+</style>
---
+++ src/wordcaingame/WordChainGame.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>{{ word }}</div>
+    <form @submit="submitForm">
+      <input type="text" ref="answer" v-model="value"/>
+      <button type="submit">입력</button>
+    </form>
+    <div>{{ result }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+// setup(){
+  data() {
+    return {
+      word: this.startWord
+      , result: ''
+      , value: ''
+    }
+  }
+  , props: {
+    startWord: String
+  }
+  , methods: {
+    submitForm(e) {
+      e.preventDefault();
+      if (this.word[this.word.length - 1] === this.value[0]) {
+        this.result = "정답";
+        this.word = this.value;
+        this.value = '';
+        this.$refs.answer.focus();
+      } else {
+        this.result = "땡";
+        this.value = '';
+        this.$refs.answer.focus();
+      }
+    }
+  }
+}
+</script>
+
+<!--<style lang="scss" scoped>-->
+<style scoped>
+
+</style>
