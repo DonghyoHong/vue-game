68c46f73b7e7d2c46f75c45e7a79b70f39b0bffa
DonghyoHong <gogoehdgy@naver.com>
2024-06-19 11:22:08 +0900

    # webpack 적용 된 App.vue -> ParentWordGame.vue로 변경

---
+++ src/components/wordcaingame/ParentWordGame.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <WordChainGame :startWord=word1></WordChainGame>
+    <WordChainGame :startWord=word2></WordChainGame>
+  </div>
+</template>
+
+<script type="module">
+import WordChainGame from "Components/wordcaingame/WordChainGame.vue";
+
+export default {
+// setup(){
+  data() {
+    return {
+      word1: "초밥"
+      , word2: "강의"
+    }
+  }
+  , components: {
+    WordChainGame
+  }
+}
+</script>
+
+<!--<style lang="scss" scoped>-->
+<style>
+
+</style>
