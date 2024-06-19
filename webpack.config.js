fbeb7799556273e98ebf6550a1ae3869df69eb68
DonghyoHong <gogoehdgy@naver.com>
2024-06-19 11:21:28 +0900

    # webpack 적용

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+    mode: "development",
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            Components: path.resolve(__dirname, './src/components/'),
+        }
+    },
+    entry: "./src/main.js"
+    , module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+
+    }
+    , plugins: [
+        new VueLoaderPlugin()
+    ]
+    , output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
