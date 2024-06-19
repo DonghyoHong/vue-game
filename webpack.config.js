e6a967d332600f3c4cd9cf3c428eafbd0e21e738
DonghyoHong <gogoehdgy@naver.com>
2024-06-19 14:20:17 +0900

    # webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const {VueLoaderPlugin} = require('vue-loader');
 module.exports = {
     mode: "development",
     resolve: {
-        extensions: [".js"],
+        extensions: [".js", ".vue"],
         alias: {
             Components: path.resolve(__dirname, './src/components/'),
         }
